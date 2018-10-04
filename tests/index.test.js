bcc7d67eb403832d3e2fad091f15e166f41e49f8
nicklaus_ <pedcremo@gmail.com>
2018-10-04 20:01:13 +0200

    index test not executed after DOM loaded

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,10 @@
 const index=require('../src/index.js');
 
-test('Primer test', () => {
-  // Set up our document body
+beforeEach(()=> {
+    // Set up our document body
   document.body.innerHTML =
   '<form>'+
-  '<select id="selectPerson" name="selectPerson">'+
+  '<select id="selectPerson" name="selectPerson">'+  
   '       <!-- TO FILL --> '+
   '</select>	<br><br>'+
   '<div id="operands">'+
@@ -21,9 +21,22 @@ test('Primer test', () => {
   
    '</form>'+
 '<h1 id="hResult">Result:</h1>';
+  
+});
+
+test('Primer test select Person list html tag is filled', () => {
   // This module has a side-effect
   const $ = require('jquery');
+  console.log("GGGGG");
+  document.addEventListener("DOMContentLoaded",()=>{
+    console.log("NUM options"+$('#selectPerson option').length);  
+  });
+  /*$().ready(() => {
+    console.log("NUM options"+$('#selectPerson option').length);
  
+  });*/
+  
+
   // Use jquery to emulate a click on our button
   
   $('#buttonNewOperand').click();
